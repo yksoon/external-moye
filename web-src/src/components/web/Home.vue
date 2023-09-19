84fe8fc572b90a1ee074b5f1c312e8b7f8a53c20
용광순 <gs.yong@hicomp.net>
2023-09-19 13:50:08 +0900

    테스트

--- web-src/src/components/web/Home.vue
+++ web-src/src/components/web/Home.vue
@@ -2,56 +2,63 @@
     <div>
         <h1>Home Page</h1>
         <md-dialog-alert
-        :md-active.sync="first"
-        md-content="Your post has been deleted!"
-        md-confirm-text="Cool!" />
+            :md-active.sync="first"
+            md-content="Your post has been deleted!"
+            md-confirm-text="Cool!"
+        />
 
         <md-dialog-alert
-        :md-active.sync="second"
-        md-title="Post created!"
-        md-content="Your post <strong>Material Design is awesome</strong> has been created." />
+            :md-active.sync="second"
+            md-title="Post created!"
+            md-content="Your post <strong>Material Design is awesome</strong> has been created."
+        />
 
-        <md-button class="md-accent md-raised" @click="first = true">Alert</md-button>
-        <md-button class="md-primary md-raised" @click="second = true">Alert</md-button>
+        <md-button class="md-accent md-raised" @click="first = true"
+            >Alert</md-button
+        >
+        <md-button class="md-primary md-raised" @click="second = true"
+            >Alert</md-button
+        >
 
-        <br /><a @click="restTest">Alert First A Tag</a>
-        <br /><a @click="second = true">Alert Second A Tag</a>
+        <br /><a @click="restTest">Alert First A Tag</a> <br /><a
+            @click="second = true"
+            >Alert Second A Tag</a
+        >
     </div>
 </template>
 
 <script>
-import { CommonRest } from '@/common/js/common';
-import { apiPath } from '@/webPath';
+import { CommonRest } from "@/common/js/common";
+import { apiPath } from "@/webPath";
 
 const restTest = () => {
+    // 테스트
     const err = {};
-    
+
     const restParams = {
-            method: "post",
-            url: apiPath.api_codes,
-            data: {},
-            err: err,
-            callback: (res) => responsLogic(res),
-        };
+        method: "post",
+        url: apiPath.api_codes,
+        data: {},
+        err: err,
+        callback: (res) => responsLogic(res),
+    };
 
     CommonRest(restParams);
 
     const responsLogic = (res) => {
-            console.log(res);
-        };
-}
+        console.log(res);
+    };
+};
 
 export default {
-    name: 'Home',
+    name: "Home",
     data: () => ({
         first: false,
         second: false,
-        restTest: restTest
-    })
-}
+        restTest: restTest,
+    }),
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
+<style scoped></style>
