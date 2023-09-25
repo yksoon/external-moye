f4f8c3a9b33c39cb60d4957e86e1b067e9a59d72
용광순 <gs.yong@hicomp.net>
2023-09-25 14:57:16 +0900

    테스트

--- web-src/src/components/admin/Signin.vue
+++ web-src/src/components/admin/Signin.vue
@@ -1,16 +1,24 @@
 <script>
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import { apiPath } from "@/webPath.js";
 import { CommonErrModule, CommonRest } from "@/common/js/common.js";
 
-export default SigninComponent({
+export default {
     name: "Signin",
     setup() {
         const err = CommonErrModule();
 
+        // 상태(State)
+        const count = ref(0);
+
         onMounted(() => {
             getCodes();
         });
+
+        // 기능(Actions)
+        function increment() {
+            count.value++;
+        }
 
         const getCodes = () => {
             // /v1/_codes
@@ -39,14 +47,28 @@ export default SigninComponent({
             };
         };
 
-        return {};
+        return {
+            count: count,
+            increment,
+        };
     },
-});
+};
 </script>
 
 <template>
     <h1>Signin Page</h1>
-    <div></div>
+    <div>
+        <span> ID : </span>
+        <span>
+            <input style="background-color: white" type="text" />
+        </span>
+    </div>
+    <div>
+        <span> PW : </span>
+        <span>
+            <input style="background-color: white" type="password" />
+        </span>
+    </div>
 </template>
 
 <!-- import 시 동일하게 scoped 옵션이 적용되어 다른 컴포넌트에 영향 없는지 테스트 해보기 -->
