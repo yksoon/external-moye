2bb5df47047d1d24b8d03784afaa4f58c665b669
DESKTOP-PKVBV1S\LG <choi.sh@hicomp.net>
2023-10-23 18:01:36 +0900

    검색버튼 css 수정 (배포)

---
+++ build/assets/People-bf6dce4c.js
@@ -0,0 +1,3 @@
+import{_ as t,c as n,d as r,e as s,F as o,f as e}from"./index-c84c5c44.js";const c={},l=e("div",{style:{height:"70vh","font-size":"2rem","text-align":"center","line-height":"70vh",color:"#666"}},[e("h1",null,[e("b",null,"준비중입니다.")])],-1);function a(i,m){return n(),r(o,null,[s(` <div style="display:flex; justify-content: center; align-items: center;">\r
+        <img src="/img/common/prepare.jpg" alt="준비중입니다.">\r
+    </div> `),l],2112)}const _=t(c,[["render",a],["__file","C:/Users/LG/Desktop/server/159/moye/frontend/web-src/src/components/web/peoples/People.vue"]]);export{_ as default};
