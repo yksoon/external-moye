85e3dc33e783609f943397ce656a435bf6150a41
nyoon <ye.jeong@hicomp.net>
2023-10-30 14:20:53 +0900

    사업소개 페이지 세팅

--- web-src/src/components/web/businessIntro/businessAreas/BusinessAreas.vue
+++ web-src/src/components/web/businessIntro/businessAreas/BusinessAreas.vue
@@ -7,27 +7,17 @@ import LeftMenu from '@/components/web/common/LeftMenu.vue';
     <div id="wrapper">
         <!-- 서브컨텐츠     //S-->
         <div id="container" class="sub_container">
-            <LeftMenu subvisual="subvisual_intro" page="ci" />
+            <LeftMenu subvisual="subvisual_business" page="category" />
             <div id="content">
                 <div id="subtitle">
-                    <h2>CI</h2>
+                    <h2>사업분야</h2>
                 </div>
-                <div class="ci">
-                    <h3>로고</h3>
-                    <div class="box">
-                        <img src="/img/web/sub/sub_logo01.png" alt="">
-                        <img src="/img/web/sub/sub_logo02.png" alt="">
-                        <img src="/img/web/sub/sub_logo03.png" alt="">
-                    </div>
+                <div class="category">
 
-                    <h3>컬러</h3>
-                    <div class="box">
-                        <img src="/img/web/sub/ci_color.png" alt="">
-                    </div>
+
                 </div>
-                <p><img src="/img/web/sub/ci_img.png" alt=""></p>
+
             </div>
-
         </div>
     </div>
 </template>
