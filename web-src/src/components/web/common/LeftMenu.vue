4a9edb963e7b3ed9ded812de9cb10a488792e674
DESKTOP-PKVBV1S\LG <choi.sh@hicomp.net>
2023-10-31 15:35:54 +0900

    현재까지 작업현황 홈페이지 반영 (배포)

--- web-src/src/components/web/common/LeftMenu.vue
+++ web-src/src/components/web/common/LeftMenu.vue
@@ -227,7 +227,7 @@ const readyAlert = () => {
                 <ul>
                     <li><a :href="routerPath.web_notices_url" :class="page === 'notice' ? 'on' : ''">공지사항</a></li>
                     <li><a :href="routerPath.web_consulting_url" :class="page === 'consulting' ? 'on' : ''">상담문의</a></li>
-                    <li v-if="state.board"><a :href="`${fileBaseUrl}${state.filePath}`">회사소개서 다운로드</a></li>
+                    <li><a :href="state.filePath !== null ? `${fileBaseUrl}${state.filePath}` : ''">회사소개서 다운로드</a></li>
                     <li><a :href="routerPath.web_photoGallery_url" :class="page === 'photo' ? 'on' : ''">포토 갤러리</a></li>
                     <li><a :href="routerPath.web_movieGallery_url" :class="page === 'movie' ? 'on' : ''">영상 갤러리</a></li>
                 </ul>
