54140d93381e38cc47d21476e7b1dc7510bdff90
nyoon <ye.jeong@hicomp.net>
2023-10-31 11:13:10 +0900

    프로모션 페이지

--- web-src/src/components/web/businessIntro/promotion/Promotion.vue
+++ web-src/src/components/web/businessIntro/promotion/Promotion.vue
@@ -1,4 +1,5 @@
 <script setup>
+import LeftMenu from '@/components/web/common/LeftMenu.vue';
 import { CommonSpinner } from '@/common/js/common';
 import { onMounted } from "vue";
 
@@ -9,7 +10,24 @@ onMounted(() => {
 
 <template>
     <!-- 예체능 시설 Promotion -->
-    <div style="height: 70vh; display: flex; justify-content: center; align-items: center;">
-        <h1 style="font-size: 2rem; font-weight: bold; color: #888;">준비중입니다.</h1>
+    <div id="wrapper">
+        <!-- 서브컨텐츠     //S-->
+        <div id="container" class="sub_container">
+            <LeftMenu subvisual="subvisual_business" page="promotion" />
+            <div id="content">
+                <div id="subtitle">
+                    <h2>예체능 시설 Promotion</h2>
+                </div>
+                <div class="promotion">
+                    <div class="img_wrap">
+                        <p><img src="/img/web/sub/promotion_img01.png" alt=""></p>
+                        <p><img src="/img/web/sub/promotion_img02.png" alt=""></p>
+                        <p><img src="/img/web/sub/promotion_img03.png" alt=""></p>
+                    </div>
+                    <h5>이러한 고민들을 모두의예체능이 해결해드립니다.</h5>
+                    <a href="" class="more_btn_blue"><span>입점문의</span></a>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
