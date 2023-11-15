55c3c88d888f2835a8548a74369e0365e7c2bcbf
DESKTOP-PKVBV1S\LG <choi.sh@hicomp.net>
2023-11-15 14:34:39 +0900

    페이지네이션 좌우 이동 아이콘 표출 오류 수정 (배포)

--- web-src/src/main.js
+++ web-src/src/main.js
@@ -21,6 +21,7 @@ import { createVuetify } from "vuetify";
 import { aliases, mdi } from "vuetify/iconsets/mdi";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import '@mdi/font/css/materialdesignicons.min.css';
 
 const vuetify = createVuetify({
     components,
